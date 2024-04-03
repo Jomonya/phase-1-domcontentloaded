1b81a9736f5ebf87f064da4885d43dc78b5ddc19
Joy <joynandako1@gmail.com>
2024-04-03 22:03:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener ("DOMContentLoaded", function() {
+    let textElement = document.getElementById("text"). textContent;
+    document.getElementById("text"). textContent = "This is really cool!";  
+});
+
+
